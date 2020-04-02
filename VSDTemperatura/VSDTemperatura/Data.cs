2a49e1e324e6b225ee01a5846c7be567b53b68c2
Laimonas Dabužinskas <laimiscl@gmail.com>
2020-04-02 23:06:33 +0300

    Add files via upload

---
+++ VSDTemperatura/VSDTemperatura/Data.cs
@@ -0,0 +1,20 @@
+﻿namespace VSDTemperatura
+{
+    class Data
+    {
+        public double zeno_BP1_5s_Mb { get; set; }
+        public double zeno_Spd_5s_Kt { get; set; }
+        public double zeno_PP_24h_Mm { get; set; }
+        public double zeno_AT_5s_C { get; set; }
+        public double zeno_gust { get; set; }
+        public double zeno_RH_5s { get; set; }
+        public double zeno_Dir_5s { get; set; }
+        public double cl31_ClH2_30s_Ft { get; set; }
+        public double cl31_ClH1_30s_Ft { get; set; }
+        public double cl31_ClH3_30s_Ft { get; set; }
+        public double sws200_PP_Mm { get; set; }
+        public double sws200_synop_code { get; set; }
+        public double sws200_Vis_1m_KM { get; set; }
+
+    }
+}
---
+++ VSDTemperatura/VSDTemperatura/Form1.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.Net;
+using System.Windows.Forms;
+
+namespace VSDTemperatura
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly string TEMP_URL = "http://www.hkk.gf.vu.lt/json.php";
+        private readonly char[] TRIM_START = { 'w', 'u', 'p', '(' };
+        private readonly char[] TRIM_END = { ')', ';' };
+        private Data _duomenys;
+        private int _total;
+        private double _temp;
+        private Timer _timer;
+        private Label _label;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+
+            _total = 0;
+            _temp = 0;
+            var label1 = new Label
+            {
+                AutoSize = true,
+                Font = new Font("Comic Sans", 12),
+                TextAlign = ContentAlignment.TopCenter,
+                Dock = DockStyle.None,
+                Text = @"Vid paros temperatura"
+            };
+            _label = new Label
+            {
+                AutoSize = false,
+                Font = new Font("Comic Sans", 30),
+                Size = new Size(100, 50),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.None
+            };
+            _label.Left = panel1.Width / 2 - _label.Width / 2;
+            _label.Top = panel1.Height / 2 - _label.Height / 2;
+            _label.Text = GetTemperature().ToString();
+            panel1.Controls.Add(_label);
+            panel1.Controls.Add(label1);
+        }
+
+        private double GetTemperature()
+        {
+            using (var w = new WebClient())
+            {
+                var json = w.DownloadString(TEMP_URL);
+                json = json.TrimStart(TRIM_START);
+                json = json.Trim();
+                json = json.TrimEnd(TRIM_END);
+
+                _duomenys = JsonConvert.DeserializeObject<Data>(json);
+                _total++;
+                _temp += _duomenys.zeno_AT_5s_C;
+            }
+
+            return _temp / _total;
+        }
+
+        public void InitTimer()
+        {
+            _timer = new Timer();
+            _timer.Tick += timer_Tick;
+            _timer.Interval = 3600000; // in miliseconds
+            _timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.Hour == 0)
+            {
+                _total = 0;
+                _temp = 0;
+            }
+            _label.Text = GetTemperature().ToString();
+        }
+    }
+}
